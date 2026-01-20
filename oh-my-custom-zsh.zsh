f34e69610f062a3a5a70a737730dfb961e2f020b
Valentino Cossar <4087384+valentinocossar@users.noreply.github.com>
2026-01-20 02:38:52 +0100

    refactor: aliases and functions

--- oh-my-custom-zsh.zsh
+++ oh-my-custom-zsh.zsh
@@ -1,3 +1,103 @@
+# macOS
+alias ll="ls -alhF"
+alias rm="${aliases[rm]:-rm} -vi"
+alias mv="${aliases[mv]:-mv} -vi"
+alias cp="${aliases[cp]:-cp} -v"
+alias grep="${aliases[grep]:-grep} --color=auto -n"
+alias hosts="vsa /etc/hosts"
+alias knownhosts="vsa ~/.ssh/known_hosts"
+alias sshconfig="vsa ~/.ssh/config"
+alias flushdns="sudo killall -HUP mDNSResponder"
+
+# Utilities
+alias myip="dig +short txt ch whoami.cloudflare @1.0.0.1"
+alias rmds="find . -name '*.DS_Store' -type f -delete"
+alias rmt="trash"
+alias weight="tree -a --du -sh"
+
+# Git
+alias gcs="git checkout staging"
+
+# ssh-agent
+alias sshls="ssh-add -L"
+alias sshdl="ssh-add -D"
+alias sshad="ssh-add --apple-use-keychain ~/.ssh/id_rsa ~/.ssh/id_ed25519 ~/.ssh/id_ed25519_personal"
+
+# Oh My Zsh and Oh My Custom Zsh
+alias ohmyzsh="cd ~/.oh-my-zsh"
+alias ohmycustomzsh="cd ~/.oh-my-custom-zsh"
+
+# Visual Studio Code
+alias vsls="code --list-extensions"
+
+# Sail
+alias sail='bash ./vendor/bin/sail'
+
+# Set default alias for workspace
+alias works='ghub'
+
+# Override cat to use bat if available
+cat() {
+  if command -v bat &>/dev/null; then
+    bat --paging=never "$@"
+  else
+    command cat "$@"
+  fi
+}
+
+# Print plist file to stdout (XML format)
+catplist() {
+  plutil -convert xml1 -o - $1
+}
+
+# Go to workspace projects
+ghub() {
+  WORKSPACE_PATH="$HOME/Workspace/GitHub.com"
+  PROJ=$1
+
+  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
+    echo "'$PROJ' is not a directory project or not exists!"
+  else
+    if [ "$PROJ" = "" ]; then
+      cd "$WORKSPACE_PATH"
+    else
+      cd "$WORKSPACE_PATH/$PROJ"
+    fi
+  fi
+  return
+}
+
+glab() {
+  WORKSPACE_PATH="$HOME/Workspace/GitLab.com"
+  PROJ=$1
+
+  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
+    echo "'$PROJ' is not a directory project or not exists!"
+  else
+    if [ "$PROJ" = "" ]; then
+      cd "$WORKSPACE_PATH"
+    else
+      cd "$WORKSPACE_PATH/$PROJ"
+    fi
+  fi
+  return
+}
+
+# Auto completions for go to projects
+_ghub() {
+  WORKSPACE_PATH="$HOME/Workspace/GitHub.com"
+  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
+  compadd -X "Select a 'GitHub.com' project:" $DIRS
+}
+compdef _ghub ghub
+
+_glab() {
+  WORKSPACE_PATH="$HOME/Workspace/GitLab.com"
+  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
+  compadd -X "Select a 'GitLab.com' project:" $DIRS
+}
+compdef _glab glab
+
 # PW - Custom password generator function
 # Usage:
 # - pw 20 (genrate random password, without special characters)
@@ -41,71 +141,10 @@ pwbcrypt() {
   echo -e "Bcrypt hash with $ROUNDS rounds (copied to clipboard): $BCRYPT_HASH"
 }
 
-# macOS
-alias ll="ls -alhF"
-alias rm="${aliases[rm]:-rm} -vi"
-alias mv="${aliases[mv]:-mv} -vi"
-alias cp="${aliases[cp]:-cp} -v"
-alias grep="${aliases[grep]:-grep} --color=auto -n"
-alias rmds="find . -name '*.DS_Store' -type f -delete"
-alias rmt="trash"
-alias hosts="vsa /etc/hosts"
-alias exports="vsa /etc/exports"
-alias knownhosts="vsa ~/.ssh/known_hosts"
-alias sshconfig="vsa ~/.ssh/config"
-alias resetls="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -seed -r -f -v -domain local -domain user -domain system"
-alias flushdns="sudo killall -HUP mDNSResponder"
-alias clearkext="sudo kextcache --clear-staging"
-alias myip="dig +short txt ch whoami.cloudflare @1.0.0.1"
-alias qrscan="zbarimg"
-alias weight="tree -a --du -sh"
-
-# Cat
-cat() {
-  if command -v bat &>/dev/null; then
-    bat --paging=never "$@"
-  else
-    command cat "$@"
-  fi
-}
-
-# Git
-alias gcs="git checkout staging"
-
-# ssh-agent
-alias sshls="ssh-add -L"
-alias sshdl="ssh-add -D"
-alias sshad="ssh-add --apple-use-keychain ~/.ssh/id_rsa ~/.ssh/id_ed25519 ~/.ssh/id_ed25519_personal"
-
-# Oh My Zsh and Oh My Custom Zsh
-alias ohmyzsh="cd ~/.oh-my-zsh"
-alias ohmycustomzsh="cd ~/.oh-my-custom-zsh"
-
-# Vagrant
-alias vau="vagrant up"
-alias vah="vagrant halt"
-alias vas="vagrant status"
-alias var="vagrant reload"
-alias vap="vagrant provision"
-alias vaup="vagrant up --provision"
-alias vass="vagrant ssh"
-alias vabu="vagrant box update"
-
-# Visual Studio Code
-alias vsls="code --list-extensions"
-
-# BeDo CLI
-alias bedo="./bedo.sh"
-
-# Sail
-alias sail='bash ./vendor/bin/sail'
-
-# Stalk
-alias stalk="bash ./stalk"
-
-# Print plist file to stdout (XML format)
-catplist() {
-  plutil -convert xml1 -o - $1
+# Benchmark shell load time
+zshbench() {
+  SHELL=${1-$SHELL}
+  for i in $(seq 1 10); do /usr/bin/time $SHELL -i -c exit; done
 }
 
 # PHP Version Manager
@@ -119,175 +158,6 @@ pvm() {
     cd "It's the same version!"
   fi
   return
-}
-
-# Go to projets
-works() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitLab CE"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-glab() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitLab.com"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-ghub() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitHub.com"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-ghubp() {
-  WORKSPACE_PATH="$HOME/Workspace/Personal - GitHub.com"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-ado() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - Azure DevOps"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-aquest() {
-  WORKSPACE_PATH="$HOME/Workspace/AQuest - GitLab CE"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-timenet() {
-  WORKSPACE_PATH="$HOME/Workspace/Timenet - GitLab CE"
-  PROJ=$1
-
-  if [ ! -d "$WORKSPACE_PATH/$PROJ" ] && [ "$WORKSPACE_PATH/$PROJ" != "" ]; then
-    echo "'$PROJ' is not a directory project or not exists!"
-  else
-    if [ "$PROJ" = "" ]; then
-      cd "$WORKSPACE_PATH"
-    else
-      cd "$WORKSPACE_PATH/$PROJ"
-    fi
-  fi
-  return
-}
-
-# Auto completions for go to projects
-_works() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitLab CE"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Beliven - GitLab CE' project:" $DIRS
-}
-compdef _works works
-
-_glab() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitLab.com"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Beliven - GitLab.com' project:" $DIRS
-}
-compdef _glab glab
-
-_ghub() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - GitHub.com"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Beliven - GitHub.com' project:" $DIRS
-}
-compdef _ghub ghub
-
-_ghubp() {
-  WORKSPACE_PATH="$HOME/Workspace/Personal - GitHub.com"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Personal - GitHub.com' project:" $DIRS
-}
-compdef _ghubp ghubp
-
-_ado() {
-  WORKSPACE_PATH="$HOME/Workspace/Beliven - Azure DevOps"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Beliven - Azure DevOps' project:" $DIRS
-}
-compdef _ado ado
-
-_aquest() {
-  WORKSPACE_PATH="$HOME/Workspace/AQuest - GitLab CE"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'AQuest - GitLab CE' project:" $DIRS
-}
-compdef _aquest aquest
-
-_timenet() {
-  WORKSPACE_PATH="$HOME/Workspace/Timenet - GitLab CE"
-  DIRS=(`ls -d "$WORKSPACE_PATH"/*/ | tr -d ' ' | xargs basename | tr '\n' ' '`)
-  compadd -X "Select a 'Timenet - GitLab CE' project:" $DIRS
-}
-compdef _timenet timenet
-
-# Benchmark shell load time
-zshbench() {
-  shell=${1-$SHELL}
-  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
 }
 
 # Quick jump into WordPress theme folder
